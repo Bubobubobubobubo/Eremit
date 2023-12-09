0e0cf89e06c06966587e3d69f1f14e425cdb7016
Raphael Forment <raphael.forment@gmail.com>
2023-12-09 12:08:53 +0100

    small progress

--- src/clock.rs
+++ src/clock.rs
@@ -5,6 +5,19 @@ use tokio::sync::Mutex;
 use tokio::time::interval;
 
 
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct ClockState {
+  pub enabled: String,
+  pub num_peers: u64,
+  pub start_stop: String,
+  pub playing: String,
+  pub tempo: f64,
+  pub beats: f64,
+  pub phase: f64,
+  pub metro: String,
+}
+
 /// Return the current unix time as a std::time::Duration
 pub fn current_unix_time() -> Duration {
   let current_unix_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
@@ -20,6 +33,7 @@ pub struct AbeLinkState {
   pub session_state: SessionState,
   pub running: bool,
   pub quantum: f64,
+  pub snapshot: Option<ClockState>
 }
 
 impl AbeLinkState {
@@ -29,6 +43,50 @@ impl AbeLinkState {
       session_state: SessionState::new(),
       running: true,
       quantum: 4.0,
+      snapshot: None
+    }
+  }
+
+  pub fn get_clock_state(&mut self) -> ClockState {
+    self.capture_app_state();
+    let time = self.link.clock_micros();
+    let enabled = match self.link.is_enabled() {
+      true => "yes",
+      false => "no ",
+    }
+    .to_string();
+    let num_peers = self.link.num_peers();
+    let start_stop = match self.link.is_start_stop_sync_enabled() {
+      true => "yes",
+      false => "no ",
+    }
+    .to_string();
+    let playing = match self.session_state.is_playing() {
+      true => "[playing]",
+      false => "[stopped]",
+    }
+    .to_string();
+    let tempo = self.session_state.tempo();
+    let beats = self.session_state.beat_at_time(time, self.quantum);
+    let phase = self.session_state.phase_at_time(time, self.quantum);
+    let mut metro = String::with_capacity(self.quantum as usize);
+    for i in 0..self.quantum as usize {
+      if i > phase as usize {
+        metro.push('O');
+      } else {
+        metro.push('X');
+      }
+    }
+
+    ClockState {
+      enabled,
+      num_peers,
+      start_stop,
+      playing,
+      tempo,
+      beats,
+      phase,
+      metro,
     }
   }
 
@@ -51,21 +109,30 @@ impl AbeLinkState {
   pub fn commit_app_state(&mut self) {
     self.link.commit_app_session_state(&self.session_state);
   }
+  
+  pub fn is_running(&self) -> bool {
+    return self.running;
+  }
+  
+  pub fn make_snapshot(&mut self) {
+    self.snapshot = Some(self.get_clock_state());
+  }
 
   pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
       // Create a recurring timer that triggers every 10ms
-      let mut interval = interval(Duration::from_millis(10));
+      let mut interval = interval(Duration::from_millis(20));
+      self.link.enable(true);
+      self.link.enable_start_stop_sync(true);
+      println!("Starting AbeLinkState run loop");
 
       // Loop that captures the session state at regular intervals
       loop {
           // Wait for the timer to trigger
           interval.tick().await;
-
-          if !self.running {
+          self.make_snapshot();
+          if !self.is_running() {
               return Ok(());
           }
-
-          self.capture_app_state();
       }
   }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(dead_code)]
+use std::clone;
 use std::error::Error;
 use mlua::Lua;
 use std::sync::{Arc, Mutex};
+use std::error::Error as StdError;
 use midir::MidiOutputConnection;
 use mlua::Result as LuaResult;
+
 mod ascii;
 mod midi;
 mod clock;
@@ -15,18 +18,15 @@ mod config;
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", ascii::BANNER);
     let cfg: config::EremitConfig = confy::load("eremit", None)?;
-    // let clock = Arc::new(Mutex::new(clock::AbeLinkState::new()));
+    let clock = Arc::new(Mutex::new(clock::AbeLinkState::new()));
     let mut abe_link_state = clock::AbeLinkState::new();
-    // Use tokio::spawn to run the run method concurrently
     let abe_link_handle = tokio::spawn(async move {
         abe_link_state.run().await.unwrap_or_else(|err| {
             eprintln!("Error in AbeLinkState run: {:?}", err);
         });
     });
-
     let mut conn_out: Arc<Mutex<Option<MidiOutputConnection>>> = Arc::new(Mutex::new(None));
     conn_out = midi::setup_midi_connection(conn_out);
-
     let mut interpreter = interpreter::Interpreter::new();
     
     // Registering somme dummy functions as a test!
@@ -34,14 +34,42 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("Hello from Rust!");
         Ok(())
     }
-    let _ = interpreter.register_function("baba", test_function);
-    let _ = interpreter.register_value("clock", 1);
-    fn get_unix_time(_lua: &Lua, _args: ()) -> LuaResult<f64> {
+    let _ = interpreter.register_function("snapshot", {
+        let cloned_clock = Arc::clone(&clock);
+        move |_lua: &Lua, _args: ()| -> LuaResult<String> {
+        cloned_clock.lock().unwrap().make_snapshot();
+        let clock_mutex = cloned_clock.lock().unwrap();
+        let snapshot = &clock_mutex.snapshot;
+        match snapshot {
+            Some(s) => {
+                let tempo = s.tempo; let beat = s.beats; let phase = s.phase;
+                let result = String::from("{ tempo = ") + &tempo.to_string() + ", beat = " + &beat.to_string() + ", phase = " + &phase.to_string() + " }";
+                Ok(result)
+            }
+            None => Err(mlua::Error::RuntimeError("No snapshot available".to_string()))
+        }
+    }});
+    let _ = interpreter.register_function("phase", {
+        let clock = Arc::clone(&clock);
+        move |_lua: &Lua, _args: ()| -> LuaResult<f64> {
+            let mut guard = clock.lock().unwrap();
+            guard.make_snapshot();
+            Ok(guard.snapshot.as_ref().unwrap().phase)
+        }
+    });
+    let _ = interpreter.register_function("beat", {
+        let clock = Arc::clone(&clock);
+        move |_lua: &Lua, _args: ()| -> LuaResult<f64> {
+            let mut guard = clock.lock().unwrap();
+            guard.make_snapshot();
+            Ok(guard.snapshot.as_ref().unwrap().beats)
+        }
+    });
+    let _ = interpreter.register_function("unix", |_lua: &Lua, _args: ()| -> LuaResult<f64> {
         Ok(clock::current_unix_time().as_secs_f64())
-    }
-    let _ = interpreter.register_function("unix", get_unix_time);
-
+    });
     let _ = interpreter.run();
     println!("{}", ascii::GOODBYE);
+    clock.lock().unwrap().running = false;
     Ok(())
 }
